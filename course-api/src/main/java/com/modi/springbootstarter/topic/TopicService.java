b76ca697d4d42fff4225c5aa04d8e917e5549395
Jay Modi <jay.modi58@gmail.com>
2018-06-06 18:45:03 -0400

    Added embedded database derby and making db operations using spring crud repo.

--- course-api/src/main/java/com/modi/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/com/modi/springbootstarter/topic/TopicService.java
@@ -1,32 +1,43 @@
 package com.modi.springbootstarter.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 
+	@Autowired
+	private TopicRepository topicRepository;
+
+	/*
 	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("spring", "spring framework", "SPRING_DES"),
 			new Topic("java", "core Java", "JAVA_DES"),
 			new Topic("javascript", "javascript framework", "JAVASCRIPT_DES")));
-
+	*/
 	public List<Topic> getAllTopics() {
+		// return topics;
+		List<Topic> topics = new ArrayList<Topic>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	public Optional<Topic> getTopic(String id) {
+		// return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id);
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		// topics.add(topic);
+		topicRepository.save(topic);
 	}
 
 	public void updateTopic(Topic topic, String id) {
 
+		/*
 		for (int i = 0; i < topics.size(); i++) {
 
 			Topic tempTopic = topics.get(i);
@@ -35,10 +46,12 @@ public class TopicService {
 				return;
 			}
 		}
+		*/
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-
-		topics.removeIf(t -> t.getId().equals(id));
+		// topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 }
